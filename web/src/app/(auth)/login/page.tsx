64d663b4651ee8e18ca6884e4405f041ce15a392
Miroslav Mlynarik <miroslav.mlynarik@gmail.com>
2025-05-09 00:54:00 +0200

    First commit

--- web/src/app/(auth)/login/page.tsx
+++ web/src/app/(auth)/login/page.tsx
@@ -6,8 +6,8 @@ export default function Login() {
         <div className="flex flex-col gap-30 items-center">
             <div className="mt-40 flex items-center justify-center gap-60">
                 <div className="flex flex-col gap-5">
-                    <h1 className="text-4xl font-bold text-sky-600">Virtual Crypto Exchange</h1>
-                    <h2 className="text-2xl font-bold">Your safe place to trade crypto</h2>
+                    <h1 className="text-5xl font-bold text-sky-600">Virtual Crypto Exchange</h1>
+                    <h2 className="text-3xl">Your safe place to trade crypto</h2>
                 </div>
                 <LoginForm />
             </div>
