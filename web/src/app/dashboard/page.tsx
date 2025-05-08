a33c527be3803f7d8292e879a06171fb32c41c5b
Miroslav Mlynarik <miroslav.mlynarik@gmail.com>
2025-05-08 09:59:39 +0200

    Change reload freq

--- web/src/app/dashboard/page.tsx
+++ web/src/app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import LogoutButton from "@/components/LogoutButton";
 export default function Dashboard() {
     return (
         <>
-            <h1>Dashboard XXX</h1>
+            <h1>VCE Dashboard</h1>
             <LogoutButton />
         </>
     );
