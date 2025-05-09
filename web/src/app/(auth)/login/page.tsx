42cfb5486d92a7e8acba3194b21da26d45a0a268
Miroslav Mlynarik <miroslav.mlynarik@gmail.com>
2025-05-09 23:01:24 +0200

    Fix position

--- web/src/app/(auth)/login/page.tsx
+++ web/src/app/(auth)/login/page.tsx
@@ -3,15 +3,15 @@ import NextImageLink from "@/components/NextImageLink";
 
 export default function Login() {
     return (
-        <div className="flex flex-col gap-30 items-center">
-            <div className="mt-45 flex items-center justify-center gap-50">
-                <div className="flex flex-col gap-5">
-                    <h1 className="text-5xl font-bold text-sky-600">Virtual Crypto Exchange</h1>
-                    <h2 className="text-3xl">Your safe place to trade crypto</h2>
-                </div>
+        <div className="flex flex-col items-center">
+            <div className="absolute start-1/5 top-2/5 flex flex-col justify-center gap-5">
+                <h1 className="text-5xl font-bold text-sky-600">Virtual Crypto Exchange</h1>
+                <h2 className="text-3xl">Your safe place to trade crypto</h2>
+            </div>
+            <div className="absolute inset-y-1/5 start-3/5">
                 <LoginForm />
             </div>
-            <footer className="fixed bottom-1/10">
+            <footer className="absolute bottom-1/10">
                 <NextImageLink />
             </footer>
         </div>
--- web/src/app/(auth)/signup/page.tsx
+++ web/src/app/(auth)/signup/page.tsx
@@ -3,15 +3,15 @@ import SignUpForm from "@/components/SignUpForm";
 
 export default function SignUp() {
     return (
-        <div className="flex flex-col items-center gap-30">
-            <div className="mt-40 flex items-center justify-center gap-50">
-                <div className="flex flex-col gap-5">
-                    <h1 className="text-5xl font-bold text-sky-600">Virtual Crypto Exchange</h1>
-                    <h2 className="text-3xl">Your safe place to trade crypto</h2>
-                </div>
+        <div className="flex flex-col items-center">
+            <div className="absolute start-1/5 top-2/5 flex flex-col justify-center gap-5">
+                <h1 className="text-5xl font-bold text-sky-600">Virtual Crypto Exchange</h1>
+                <h2 className="text-3xl">Your safe place to trade crypto</h2>
+            </div>
+            <div className="absolute inset-y-2/11 start-3/5">
                 <SignUpForm />
             </div>
-            <footer className="fixed bottom-1/10">
+            <footer className="absolute bottom-1/10">
                 <NextImageLink />
             </footer>
         </div>
