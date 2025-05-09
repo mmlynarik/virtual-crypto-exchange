8164a172b22b9cc3042bec750a8b0a3248c4a0cd
Miroslav Mlynarik <miroslav.mlynarik@gmail.com>
2025-05-09 12:30:09 +0200

    New colors

--- web/src/app/(auth)/login/page.tsx
+++ web/src/app/(auth)/login/page.tsx
@@ -4,7 +4,7 @@ import NextImageLink from "@/components/NextImageLink";
 export default function Login() {
     return (
         <div className="flex flex-col gap-30 items-center">
-            <div className="mt-40 flex items-center justify-center gap-50">
+            <div className="mt-45 flex items-center justify-center gap-50">
                 <div className="flex flex-col gap-5">
                     <h1 className="text-5xl font-bold text-sky-600">Virtual Crypto Exchange</h1>
                     <h2 className="text-3xl">Your safe place to trade crypto</h2>
